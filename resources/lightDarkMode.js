295e8101ff5a4a333bb3bbf5b99ccc8df4dd2709
kyleyoshimoto <107731465+kyleyoshimoto@users.noreply.github.com>
2023-10-25 22:11:37 -0700

    Implemented working dark/light theme button
    Designed and positioned a box with two check marks that say light and dark. Made interactive with lightDarkMode.js

---
+++ resources/lightDarkMode.js
@@ -0,0 +1,52 @@
+
+// Function the checks if system preferences match or not and returns appropriate theme.
+function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark }) {
+    if (localStorageTheme !== null) {
+        return localStorageTheme;
+    } else if (systemSettingDark.matches) {
+        return "dark";
+    } else {
+        return "light";
+    }
+};
+
+// Updates the button accordingly to what mode is currently enabled.
+function updateButton({buttonEl, isDark}) {
+    if (isDark) {
+        document.getElementById("dark").style.background = "var(--accentColor)";
+        document.getElementById("light").style.background = "var(--backgroundColor)";
+    } else {
+        document.getElementById("light").style.background = "var(--accentColor)";
+        document.getElementById("dark").style.background = "var(--backgroundColor)";
+    }
+};
+
+
+// Updates theme on html tag.
+function updateHtml({theme}) {
+    document.querySelector("html").setAttribute("data-theme", theme);
+};
+
+// Points to actual button on html doc.
+const button = document.querySelector("[data-theme-toggle]")
+// Obtain access to a localStorage value that will later allow us to make the change accordingly.
+const localStorageTheme = localStorage.getItem("theme");
+// Checks to see if system preferences match.
+const systemSettingDark = window.matchMedia("(perfers-color-scheme: dark)");
+
+// Constant for current setting.
+let currentThemeSetting = calculateSettingAsThemeString({localStorageTheme, systemSettingDark});
+
+// Calls our functions to update theme setting and button display.
+updateButton({buttonEl: button, isDark: currentThemeSetting === "light"});
+updateHtml({theme: currentThemeSetting});
+
+button.addEventListener("mousedown", (event) => {
+    const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
+
+    localStorage.setItem("theme", newTheme);
+    updateButton({buttonEl: button, isDark: newTheme === "dark"});
+    updateHtml({theme: newTheme});
+
+    currentThemeSetting = newTheme;
+});
